2dd64cb5e4db4207fae7836be7b8b401d500efa2
Shrijeet16 <39207849+Shrijeet16@users.noreply.github.com>
2019-08-10 10:56:37 +0530

    Delete plothist.m

--- plothist.m
+++
@@ -1,7 +0,0 @@
-function [ h ] = plothist( A )
-%CLASSHISTOGRAM Summary of this function goes here
-%   Detailed explanation goes here
-edges = [-1:0.1 :8];
-h = histogram(A , edges , 'Normalization','pdf');
-end
-
